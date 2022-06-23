d6a45bcddf841b1e760bf64c3af6b77b78101299
tonia <tonia@cia.como>
2022-06-23 15:30:42 +0200

    fine consegna base

--- src/jana60calcolatrice/CalcoliHelper.java
+++ src/jana60calcolatrice/CalcoliHelper.java
@@ -1,5 +1,74 @@
 package jana60calcolatrice;
 
-public class CalcoliHelper {
+import java.util.Random;
 
+public class CalcoliHelper {
+/*Creare una classe di helper CalcoliHelper. Come visto a lezione, strutturare la classe in modo che non possa essere istanziata (cioè non posso creare oggetti di tipo CalcoliHelper con la parola chiave new) e che presenti i seguenti metodi static:
+- Somma di due numeri interi
+- Differenza tra due numeri interi
+- Moltiplicazione di due numeri interi
+- Valore assoluto di un numero intero
+- Minimo tra due numeri interi
+- Massimo tra due numeri interi*/
+	
+	private CalcoliHelper() {
+		//costruttore privato
+	}
+	
+	//somma
+	public static int somma(int a, int b) {
+		return a+b;
+	}
+	
+	//differenza
+	public static int differenza(int c, int d) {
+		return c - d;
+	}
+	//differenza 2 
+	public static int differenza2(int c2, int d2) {
+		if(c2 < d2) {
+			return d2 - c2;
+		}else {
+			return d2 - c2;
+		}
+	}
+	
+	//moltiplicazione
+	public static int moltiplicazione(int e, int f) {
+		return e * f;
+	}
+	
+	//valore assoluto
+	public static int valoreAssoluto(int h) {
+		if(h<0) {
+			h=h;
+		}
+		return h;
+	}
+	
+	//minimo tra 2 numeri
+	public static int numeroMinimo(int i, int l) {
+		int minimo;
+		if(i < l) {
+			minimo= i;
+		}else {
+			minimo = l;
+		}
+		return minimo;
+	}
+	
+
+	//massimo tra due numeri
+	public static int numeroMassimo(int r, int r2) {
+		int massimo;
+		if(r > r2) {
+			massimo= r;
+		}else {
+			massimo = r2;
+		}
+		return massimo;
+	}
+		
 }
+	
+
--- src/jana60calcolatrice/CalcoliTest.java
+++ src/jana60calcolatrice/CalcoliTest.java
@@ -1,5 +1,17 @@
 package jana60calcolatrice;
 
+
 public class CalcoliTest {
+/*Create anche una classe CalcoliTest con il metodo main in cui testate tutti i metodi della 
+ * vostra classe di helper (con un esempio per ogni casistica)*/
+
+	public static void main(String[] args) {
+		
+		System.out.println(CalcoliHelper.somma(4, 6));
+		System.out.println(CalcoliHelper.differenza(8,4));
+		System.out.println(CalcoliHelper.moltiplicazione(5, 6));
+		System.out.println(CalcoliHelper.numeroMinimo(10, 8));
+		System.out.println(CalcoliHelper.numeroMassimo(20, 30));
+	}
 
 }
